1a03c6ebae3c6fb75dcadeb69c7231c24ed1e6ef
ospodaryk <ospodaryk11@gmail.com>
2023-05-26 19:05:03 +0300

    Added Role service and impl

---
+++ src/main/java/com/project/mypetproject_library/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.project.mypetproject_library.repository;
+
+import com.project.mypetproject_library.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
