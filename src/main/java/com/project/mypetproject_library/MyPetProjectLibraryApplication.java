bc5f0dea523505d4a2d769280e1889d6bbefb246
ospodaryk <ospodaryk11@gmail.com>
2023-05-26 13:27:16 +0300

    Init

---
+++ src/main/java/com/project/mypetproject_library/MyPetProjectLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.project.mypetproject_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetProjectLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetProjectLibraryApplication.class, args);
+    }
+
+}
